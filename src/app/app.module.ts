f2f2d4eab57bb419e1c45654b8c71211aef15574
Mangesh <dholem77@gmail.com>
2023-12-18 10:08:21 +0530

    Revert "2nd commit created dashboard and navbar component"
    This reverts commit 63d44fb7e5d5bc1b015f9441e2214fde778b9e99.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
